fd877cafa7de51f3b06fef39d742e29305dc79ea
doniel <donielacosta1995@gmail.com>
2023-05-09 10:41:34 -0400

    distintas formas para calcular el promedio

--- mathP.js
+++ mathP.js
@@ -1,9 +1,34 @@
+// function calcularPromedio(lista){
+//     let  sumaList = 0;
+//     for(let i = 0; i < lista.length; i++){
+//         sumaList = sumaList + lista[i];
+//     }
+
+//     const promedio = sumaList / lista.length;
+//     console.log(promedio);
+//     return promedio;
+// }
+
+    // metodo reduce
+
+// function calcularPromedio(lista){
+//     function sumar(valorAcumulado, nuevoValor){
+//         return valorAcumulado + nuevoValor;
+//     }
+//     const sumaList = lista.reduce(sumar);
+//     const promedio = sumaList / lista.length;
+//     console.log(promedio);
+//     return promedio;
+// }
+
 function calcularPromedio(lista){
-    let  sumaList = 0;
-    for(let i = 0; i < lista.length; i++){
-        sumaList = sumaList + lista[i];
-    }
-    const promedio = sumaList/lista.length;
+    //arror function
+    const sumar = (valorAcumulado, nuevoValor)=>{
+       return valorAcumulado + nuevoValor;
+    };
+    const sumaList = lista.reduce(sumar);
+
+    const promedio = sumaList / lista.length;
     console.log(promedio);
     return promedio;
 }
